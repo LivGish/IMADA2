00ecf37ca2fb9b3e99eab18078a8c51ee4638940
Olivia Guichard <ST10486576@vcconnect.edu.za>
2025-05-20 14:53:42 +0200

    MainActivity.kt completed (code added to display the app description and give the button a function)

--- app/src/main/java/com/example/imada2/MainActivity.kt
+++ app/src/main/java/com/example/imada2/MainActivity.kt
@@ -4,6 +4,7 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,7 +17,24 @@ class MainActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
 
+        val btnStart = findViewById<Button>(R.id.btnStart)
 
+        val txtBody = findViewById<TextView>(R.id.txtBody)
+
+        txtBody.text = """
+            Welcome to the physics flashcard 
+            app!
+            There are 5 true or false 
+            questions based on waves in 
+            physics.
+            Press 'start' to begin the quiz. 
+        """.trimIndent()
+
+        btnStart.setOnClickListener{
+            val intent = Intent (this, flashcards::class.java)
+            startActivity(intent)
+
+        }
 
     }
 }

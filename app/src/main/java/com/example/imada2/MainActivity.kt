f9db018b76432b84ab79fbba197b54bd418c907c
Olivia Guichard <ST10486576@vcconnect.edu.za>
2025-05-20 12:40:06 +0200

    activity_main.xml completed (design aspects of welcome page)

---
+++ app/src/main/java/com/example/imada2/MainActivity.kt
@@ -0,0 +1,22 @@
+package com.example.imada2
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+
+
+    }
+}

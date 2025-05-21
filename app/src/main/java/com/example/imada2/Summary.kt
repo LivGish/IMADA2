74a33382b76120c49b735a81bafc757e3238e283
Olivia Guichard <ST10486576@vcconnect.edu.za>
2025-05-21 12:19:23 +0200

    activity_summary.xml updated and Summary.kt started (review button added + some variables declared)

--- app/src/main/java/com/example/imada2/Summary.kt
+++ app/src/main/java/com/example/imada2/Summary.kt
@@ -1,6 +1,8 @@
 package com.example.imada2
 
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -11,6 +13,14 @@ class Summary : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_summary)
+
+        //declare variables to be used in the code
+        val btnExit = findViewById<Button>(R.id.btnExit)
+
+        //declare variables to be used in the code
+        val txtBody = findViewById<TextView>(R.id.txtBody)
+
+
 
     }
 }
